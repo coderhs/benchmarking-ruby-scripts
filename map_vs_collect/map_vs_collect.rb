7b26b69dc2174b3a105cc4009267aaed1461c518
coderhs <mailme@hsps.in>
2015-04-28 08:14:34 +0530

    Change code to benchmark/ips

--- map_vs_collect/map_vs_collect.rb
+++ map_vs_collect/map_vs_collect.rb
@@ -3,20 +3,13 @@ require 'benchmark/ips'
 
 
 Benchmark.ips do |r|
-  N = 100000
-
+  a = [1, 2, 3, 4, 5]
   r.report('collect') do
-    a = [1, 2, 3, 4, 5]
-    N.times do
-      b = a.collect { |x| x*x }
-    end
+    a.collect { |x| x*x }
   end
 
   r.report('map') do
-    a = [1, 2, 3, 4, 5]
-    N.times do
-      b = a.map { |x| x*x }
-    end
+    a.map { |x| x*x }
   end
   r.compare!
 end
