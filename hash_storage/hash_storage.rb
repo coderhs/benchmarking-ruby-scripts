771a1aa74c642575d99c7a7749be5ab056bcd284
coderhs <mailme@hsps.in>
2015-04-28 08:14:19 +0530

    Changed code to benchmark/ips

--- hash_storage/hash_storage.rb
+++ hash_storage/hash_storage.rb
@@ -1,23 +1,23 @@
 require 'benchmark'
+require 'benchmark/ips'
 
-Benchmark.bm do |x|
-  x.report do
-    1000000.times do
-      my_hash = {}
-      my_hash[:t] = 1
-      my_hash[:b] = 2
-    end
+Benchmark.ips do |r|
+  N = 100000
+  a = [1, 2, 3, 4, 5]
+  r.report('{}') do
+    my_hash = {}
+    my_hash[:t] = 1
+    my_hash[:b] = 2
   end
-  x.report do
-    1000000.times do
-      my_hash = {}
-      my_hash.store(:t, 1)
-      my_hash.store(:b, 2)
-    end
+
+  r.report('store') do
+    my_hash = {}
+    my_hash.store(:t, 1)
+    my_hash.store(:b, 2)
   end
-  x.report do
-    1000000.times do
-      my_hash = {:t => 1, :t => 2}
-    end
+
+  r.report('{:a => 1}') do
+    my_hash = {:t => 1, :t => 2}
   end
+  r.compare!
 end
